26c9f1b32196364d775f5b7d116a4a29454e64e9
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-10-05 19:54:04 +0800

    verndo

--- absensi.sql
+++ absensi.sql
@@ -1,41 +1,107 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Localhost MySQL
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50733
- Source Host           : localhost:3306
+ Source Server Version : 80029
+ Source Host           : localhost:55000
  Source Schema         : absensi
 
  Target Server Type    : MySQL
- Target Server Version : 50733
+ Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 05/10/2022 14:02:39
+ Date: 05/10/2022 19:46:33
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for anggota
+-- ----------------------------
+DROP TABLE IF EXISTS `anggota`;
+CREATE TABLE `anggota` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  `uid` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3;
+
+-- ----------------------------
+-- Records of anggota
+-- ----------------------------
+BEGIN;
+INSERT INTO `anggota` VALUES (2, 'Andi Fajar', 'fajar', '2022-10-05 11:26:51', '2022-10-05 11:26:51');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for kehadiran
+-- ----------------------------
+DROP TABLE IF EXISTS `kehadiran`;
+CREATE TABLE `kehadiran` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `anggota_id` bigint DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `anggota_id` (`anggota_id`),
+  CONSTRAINT `kehadiran_ibfk_1` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- ----------------------------
+-- Records of kehadiran
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for pelacakan
+-- ----------------------------
+DROP TABLE IF EXISTS `pelacakan`;
+CREATE TABLE `pelacakan` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `anggota_id` bigint DEFAULT NULL,
+  `long` varchar(255) DEFAULT NULL,
+  `lat` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `anggota_id` (`anggota_id`),
+  CONSTRAINT `pelacakan_ibfk_1` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2;
+
+-- ----------------------------
+-- Records of pelacakan
+-- ----------------------------
+BEGIN;
+INSERT INTO `pelacakan` VALUES (1, 2, '123', '321', '2022-10-05 11:43:59', '2022-10-05 11:43:59');
+COMMIT;
+
+-- ----------------------------
 -- Table structure for pengguna
 -- ----------------------------
 DROP TABLE IF EXISTS `pengguna`;
-CREATE TABLE `pengguna`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `uid` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `kata_sandi` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `level` tinyint(4) NULL DEFAULT NULL,
-  `remember_token` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+CREATE TABLE `pengguna` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255)  DEFAULT NULL,
+  `uid` varchar(255)  DEFAULT NULL,
+  `kata_sandi` varchar(255)  DEFAULT NULL,
+  `level` tinyint DEFAULT NULL,
+  `remember_token` varchar(255)  DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of pengguna
 -- ----------------------------
-INSERT INTO `pengguna` VALUES (1, 'Administrator', 'admin', '$2y$10$n4RaD2tZJWOmqk.IXe/22OUuWHFN5/AJN8DfgCjGqP68NCNB3ErE6', 1, NULL, '2022-10-05 00:44:39', '2022-10-05 05:52:59');
+BEGIN;
+INSERT INTO `pengguna` VALUES (1, 'Administrator', 'admin', '$2y$10$xgzwJnE4jr9jhPzVw7SCBeCEUv413nGud2CyQyTShZBuzbivUN.Cm', 1, NULL, '2022-10-05 00:44:39', '2022-10-05 09:39:57');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
